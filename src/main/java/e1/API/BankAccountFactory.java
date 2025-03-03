5b78ff70ca2490630a77a922b8515da1d0210ab3
gioele santi <93931915+Saint138@users.noreply.github.com>
2025-03-03 19:32:04 +0100

    Corrected error

--- src/main/java/e1/API/BankAccountFactory.java
+++ src/main/java/e1/API/BankAccountFactory.java
@@ -3,5 +3,5 @@ package e1.API;
 public interface BankAccountFactory {
 
     //For create different types of accounts
-    BankAccount createSilverAccount(int fee);
+    BankAccount createSilverAccount();
 }
